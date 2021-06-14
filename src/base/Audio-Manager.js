a82f78bfd0da1328cf8e9417c486889432c5913c
tpro1234 <59308474+tpro1234@users.noreply.github.com>
2021-06-14 22:35:27 +0300

    Update Audio-Manager.js

--- src/base/Audio-Manager.js
+++ src/base/Audio-Manager.js
@@ -12,7 +12,7 @@ module.exports = async (bot) => {
 
 	bot.manager = new Manager({
 		nodes: [
-			{ host: 'https://lavalink1121.herokuapp.com/', port: 5000, password: 'youshallnotpass' },
+			{ host: 'lavalink1121.herokuapp.com', port: 5000, password: 'youshallnotpass' },
 		],
 		plugins: [
 			new Spotify({ clientID, clientSecret }),
