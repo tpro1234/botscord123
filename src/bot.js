0b0058e6ae892273bf363969620289ca3e464e95
tpro1234 <59308474+tpro1234@users.noreply.github.com>
2021-06-15 00:22:21 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -28,7 +28,6 @@ app.get('/', (req, res) => {
   res.json('This bot should be online! Uptimerobot will keep it alive');
 });
 
-app.listen(process.env.PORT);
 // Load commands
 (async () => {
 	// load commands
