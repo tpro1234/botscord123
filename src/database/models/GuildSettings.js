318ce7e810fcc8ac5bad9ba9a2364d57fedd21a6
tpro1234 <59308474+tpro1234@users.noreply.github.com>
2021-06-16 12:33:59 +0300

    Update GuildSettings.js

--- src/database/models/GuildSettings.js
+++ src/database/models/GuildSettings.js
@@ -5,12 +5,12 @@ const guildSchema = Schema({
 	guildName: String,
 	prefix: { type: String, default: require('../../config.js').defaultSettings.prefix },
 	// Welcome Plugin
-	welcomePlugin: { type: Boolean, default: false },
+	welcomePlugin: { type: Boolean, default: true },
 	// if anti-raid is true and welcome plugin is true both will get activated so this will make sure anti-riad runs first and once 'accepeted' welcome plugn will run
 	welcomeRaidConnect: { type: Boolean, default: false },
 	welcomeMessageToggle: { type: Boolean, default: false },
-	welcomeMessageChannel: { type: String, default: '00' },
-	welcomeMessageText: { type: String, default: 'Hello {user}, Welcome to **{server}**!' },
+	welcomeMessageChannel: { type: String, default: '830041375310610453' },
+	welcomeMessageText: { type: String, default: 'Hello {user}, Welcome to **{server}**! *img* ' },
 	welcomePrivateToggle: { type: Boolean, default: false },
 	welcomePrivateText: { type: String, default: 'Have a great time here in **{server}**.' },
 	welcomeRoleToggle: { type: Boolean, default: false },
@@ -18,23 +18,23 @@ const guildSchema = Schema({
 	welcomeGoodbyeToggle: { type: Boolean, default: false },
 	welcomeGoodbyeText: { type: String, default: '**{user}** just left the server.' },
 	// 0 = no announcement, 1 = reply, 2 = choosen channel
-	LevelOption: { type: Number, default: 1 },
-	LevelChannel: { type: String, default: '00' },
-	LevelMessage: { type: String, default: 'GG {user}, you have leveled up to {level}!' },
+	LevelOption: { type: Number, default: 2 },
+	LevelChannel: { type: String, default: '830431879458717727' },
+	LevelMessage: { type: String, default: 'GG {user}, you have leveled up to {level}! Checkout our store in <#848972650608590848> to get your skin' },
 	LevelIgnoreRoles: { type: Array, default: ['No-xp'] },
 	LevelIgnoreChannel: { type: Array, default: ['No-xp'] },
 	LevelMultiplier: { type: Number, default: 1 },
 	LevelRoleRewards: { type: Array, default: ['gf'] },
 	// Music plugin
-	MusicDJ: { type: Boolean, default: false },
-	MusicDJRole: { type: String, default: '00' },
+	MusicDJ: { type: Boolean, default: true },
+	MusicDJRole: { type: String, default: '854654002985566258' },
 	// Music trivia plugin
 	MusicTriviaPlugin: { type: Boolean, default: false },
 	MusicTriviaGenres: { type: Array, default: ['pop'] },
 	// logging plugin
-	ModLog: { type: Boolean, default: false },
+	ModLog: { type: Boolean, default: true },
 	ModLogEvents: { type: Array, default: ['GUILDBANADD', 'GUILDMEMBERADD'] },
-	ModLogChannel: { type: String, default: '00' },
+	ModLogChannel: { type: String, default: '854117644885819402' },
 	ModLogIgnoreBot: { type: Boolean, default: true },
 	// CommandCooldown
 	CommandChannelToggle: { type: Boolean, default: false },
@@ -43,7 +43,7 @@ const guildSchema = Schema({
 	CommandCooldownSec: { type: Number, default: 2 },
 	// Moderation plugin
 	ModeratorRoles: { type: Array, default: ['owner'] },
-	MutedRole: { type: String, default: '00' },
+	MutedRole: { type: String, default: '830080261038276649' },
 	automoderation: Schema.Types.Mixed,
 	// How many warnings till the user is kicked from server
 	ModerationWarningCounter: { type: Number, default: 3 },
@@ -53,15 +53,15 @@ const guildSchema = Schema({
 	ModerationIgnoreBotToggle: { type: Boolean, default: true },
 	// For ticket command
 	TicketToggle: { type: Boolean, default: true },
-	TicketSupportRole: { type: String, default: '00' },
-	TicketCategory: { type: String, default: '00' },
+	TicketSupportRole: { type: String, default: '830536100111450153' },
+	TicketCategory: { type: String, default: '830035790066614323' },
 	// for report command
 	ReportToggle: { type: Boolean, default: true },
 	// Anti-raid plugin
-	AntiRaidPlugin: { type: Boolean, default: false },
+	AntiRaidPlugin: { type: Boolean, default: true },
 	// 0 = nothing, 1 = verify account by reacting to message, 2 = must complete CAPTCHA to join
-	AntiRaidCompletion: { type: Number, default: 0 },
-	AntiRaidChannelID: { type: String, default: '00' },
+	AntiRaidCompletion: { type: Number, default: 2 },
+	AntiRaidChannelID: { type: String, default: '854362654260199444' },
 	// Search PLugin
 	// Misc
 	DisabledCommands: { type: Array, default: ['IWannaHackYou'] },
