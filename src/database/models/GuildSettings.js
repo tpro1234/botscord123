6cb1f781e72e02d26cf0779b9d3d172556ea613f
tpro1234 <59308474+tpro1234@users.noreply.github.com>
2021-06-21 12:50:47 +0300

    Update GuildSettings.js

--- src/database/models/GuildSettings.js
+++ src/database/models/GuildSettings.js
@@ -10,7 +10,7 @@ const guildSchema = Schema({
 	welcomeRaidConnect: { type: Boolean, default: false },
 	welcomeMessageToggle: { type: Boolean, default: true },
 	welcomeMessageChannel: { type: String, default: '830041375310610453' },
-	WelcomeMessageText: { type: Array, default: ['GUILDBANADD', 'GUILDMEMBERADD'] },
+	WelcomeMessageText: { type: String, default: 'Have a great time **{server}** {tag}' },
 	welcomePrivateToggle: { type: Boolean, default: false },
 	welcomePrivateText: { type: String, default: 'Have a great time here in **{server}**.' },
 	welcomeRoleToggle: { type: Boolean, default: false },
