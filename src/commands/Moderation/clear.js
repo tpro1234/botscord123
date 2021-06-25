d51ead565442ba1f53b820382b32a18eb1a49d61
tpro1234 <59308474+tpro1234@users.noreply.github.com>
2021-06-25 10:59:46 +0300

    Update clear.js

--- src/commands/Moderation/clear.js
+++ src/commands/Moderation/clear.js
@@ -33,10 +33,10 @@ module.exports = class Clear extends Command {
 		if (isNaN(amount) || (amount > 1000) || (amount < 1)) return message.channel.error('misc:INCORRECT_FORMAT', { EXAMPLE: settings.prefix.concat(message.translate('moderation/clear:USAGE')) }).then(m => m.delete({ timeout: 5000 }));
 
 		// make sure guild is premium if amount > 200
-		if (amount > 200 && !message.guild.premium) return message.channel.error('moderation/clear:NO_PREM');
+		if (amount > 2000 && !message.guild.premium) return message.channel.error('moderation/clear:NO_PREM');
 
 		// Confirmation for message deletion over 100
-		if (amount >= 100) {
+		if (amount >= 1000) {
 			const embed = new Embed(bot, message.guild)
 				.setTitle(message.translate('moderation/clear:TITLE'))
 				.setDescription(message.translate('moderation/clear:DESC', { NUM: amount }));
